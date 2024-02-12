ab8621d527e695d319fad96f7ee3a3a3100a4eaa
shamilakshmanan <shamilakshmanan@gmail.com>
2024-02-12 03:06:15 -0500

    likedsong page

---
+++ react-bp/src/pages/LikedSongs/likedsong.js
@@ -0,0 +1,10 @@
+import * as React from "react";
+import { useState, useEffect } from "react";
+import "./likedsongStyle.css"
+
+function LikedSongs(){
+  return(
+    <div className="LikedsongBody">liked songs</div>
+  );
+};
+export default LikedSongs
--- react-bp/src/utilities/registry.js
+++ react-bp/src/utilities/registry.js
@@ -2,6 +2,7 @@
 import Home from "Pages/Home";
 import Login from "Pages/Login/login";
 import Signup from "Pages/Signup/signup";
+import LikedSongs from "Pages/LikedSongs/likedsong";
 
 function Injector(component, name) {
   const hoc = component;
@@ -14,4 +15,5 @@ export default {
   Login: Injector(Login, "Login"),
   Signup: Injector(Signup,"Signup"),
   Home: Injector(Home, "Home"),
+  LikedSongs: Injector(LikedSongs, "LikedSongs"),
 };
