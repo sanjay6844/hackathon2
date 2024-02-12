de8593cc81b5ae3801bf7fc1603228250570a9b5
sanjayram.a <sanjayram.a@sudoboat.com>
2024-02-12 18:33:49 +0530

    merge

--- react-bp/src/pages/LikedSongs/likedsong.js
+++ react-bp/src/pages/LikedSongs/likedsong.js
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { useState, useEffect } from "react";
+import { useState, useEffect ,useContext} from "react";
 import "./likedsongStyle.css";
 import MusicNoteIcon from "@mui/icons-material/MusicNote";
 import FavoriteIcon from "@mui/icons-material/Favorite";
@@ -7,6 +7,7 @@ import Divider from "@mui/material/Divider";
 import GraphicEqIcon from "@mui/icons-material/GraphicEq";
 import PlayArrowIcon from "@mui/icons-material/PlayArrow";
 import RefContext from "Utilities/refContext";
+import Cookies from "js-cookie";
 
 
 function LikedSongs(){
@@ -14,35 +15,63 @@ function LikedSongs(){
   const ctx = useContext(RefContext);
   const { store, actions } = ctx;
   const { postUsers,fetchUsers,fetchSongs } = actions;
+  const LoggedInUser = Cookies.get("currentuser")
   useEffect(()=>{
     fetchSongs()
     fetchUsers()
   },[])
+  const likedSongs = ["lsitpo81","lsiu6yak"]
+  let loggedInUser = JSON.parse(LoggedInUser)
+  const showLikedSongs = ()=>{
+    // const likedSongs = loggedInUser.likedSongs
+   
+    
+   
+
+    // for(let i=0;i<likedSongs?.length;i++){
+    //   for(let j=0;j<store.songs?.length;j++){
+    //     if(likedSongs[i]===store.songs[j].id){
+    //       arr.push(
+    //         <div key={i}>
+    //           <div className="likedsongdesign">
+    //             <div className="likedsonglist"><MusicNoteIcon style={{padding:"10px", borderRadius:"25px", border:"1px solid gray", fontSize:"20px",color:"white", background:"#9147ff"}} /></div>
+    //             <div className="SongTitle">{store.songs[j].songName}</div>
+    //             <div className="playbutton"><PlayArrowIcon style={{fontSize:"40px", cursor:"pointer"}} /></div>
+    //             <div><FavoriteIcon  onClick={() => setLiked(true)} style={{color: isliked ? "black" : "red", fontSize:"30px",cursor:"pointer"}} /></div>
+    //             <Divider />
+    //           </div>
+              
+    //         </div>
+    //       )
+        
+
+    //     }
+    //   }
+    // }
+
+   
+  }
 
   return(
     <div className="LikedsongBody">
-      <div className="title">Liked Songs</div>
-      <div className="likedsongdesign">
-        <div className="likedsonglist"><MusicNoteIcon style={{padding:"10px", borderRadius:"25px", border:"1px solid gray", fontSize:"20px",color:"white", background:"#9147ff"}} /></div>
-        <div className="SongTitle">Song title</div>
-        <div className="playbutton"><PlayArrowIcon style={{fontSize:"40px", cursor:"pointer"}} /></div>
-        <div><FavoriteIcon  onClick={() => setLiked(true)} style={{color: isliked ? "black" : "red", fontSize:"30px",cursor:"pointer"}} /></div>
-      </div>
-      <Divider />
-      <div className="likedsongdesign">
-        <div className="likedsonglist"><MusicNoteIcon style={{padding:"10px", borderRadius:"25px", border:"1px solid gray", fontSize:"20px",color:"white", background:"#9147ff"}} /></div>
-        <div className="SongTitle">Song title</div>
-        <div className="playbutton"> <GraphicEqIcon style={{fontSize:"40px"}} /></div>
-        <div><FavoriteIcon onClick={() => setLiked(true)} style={{color: isliked ? "black" : "red", fontSize:"30px"}} /></div>
-      </div>
-      <Divider />
-      <div className="likedsongdesign">
-        <div className="likedsonglist"><MusicNoteIcon style={{padding:"10px", borderRadius:"25px", border:"1px solid gray", fontSize:"20px",color:"white", background:"#9147ff"}} /></div>
-        <div className="SongTitle">Song title</div>
-        <div className="playbutton"><PlayArrowIcon style={{fontSize:"40px"}} /></div>
-        <div><FavoriteIcon onClick={() => setLiked(true)} style={{color: isliked ? "black" : "red", fontSize:"30px"}} /></div>
-      </div>
-      <Divider />
+      {/* {showLikedSongs()} */}
+      { store.songs?.map((song,key)=>{
+        if(likedSongs.includes(song.id)&&song.show){
+          return(
+            <div key={key}>
+              <div className="likedsongdesign">
+                <div className="likedsonglist"><MusicNoteIcon style={{padding:"10px", borderRadius:"25px", border:"1px solid gray", fontSize:"20px",color:"white", background:"#9147ff"}} /></div>
+                <div className="SongTitle">{song.songName}</div>
+                <div className="playbutton"><PlayArrowIcon style={{fontSize:"40px", cursor:"pointer"}} /></div>
+                <div><FavoriteIcon  onClick={() => setLiked(true)} style={{color: isliked ? "black" : "red", fontSize:"30px",cursor:"pointer"}} /></div>
+                <Divider />
+              </div>
+            </div>
+          )
+         
+
+        }
+      })}
     </div>
   );
 }
